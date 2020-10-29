b92a7b884d1ca88f1774b37a09b998fec53af62b
Paul Murray <paulmurray85@gmail.com>
2020-10-29 10:05:53 -0700

    html routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,38 +1,43 @@
 
 const Workout = require("../models/workout.js");
 
-app.post("/submit", ({ body }, res) => {
-    // table name ".create"
-    
-    const book = body;
-  
-    // If we want the object to have a boolean value of false,
-    // we have to do it here, because the ajax post will convert it
-    // to a string instead of a boolean
-    book.read = false;
-  });
-  
-  // Find all books marked as read
-  app.get("/api/workouts", (req, res) => {
-      // collection name.find()
-      Workout.find({}, (err, data) => {
-          if (err) {
-            console.log(err);
-          } else {
-            res.json(data);
-          }
-        });
-      });
-  
-  
-  
-  // Mark a book as having been read
-  app.put("/api/workouts/:id", (req, res) => {
-    // find by id and update
-  });
-  
-  // Mark a book as having been not read
-  app.put("/markunread/:id", (req, res) => {
-    // Remember: when searching by an id, the id needs to be passed in
-    // as (mongojs.ObjectId(IdYouWantToFind))
-  });
+
+app.post("/api/workouts", function (req, res) {
+    var workout = new Workout(req.body)
+
+    workout.save(err => {
+        if (err) return res.status(500).send(err);
+        return res.status(200).send(workout);
+    })
+})
+
+app.get("/api/workouts", function (req, res) {
+
+    Workout.find({}).then(function (data, err) {
+        if (err) throw err
+        res.send(data)
+    })
+})
+
+app.get("/api/workouts/:id", function (req, res) {
+
+    .FINDONE GOES HERE
+})
+
+
+
+
+app.put("/api/workouts/", function (req, res) {
+    var query = req.body
+    Workout.update({}, { sort: { name: 1 } }).then(function (data, err) {
+
+        console.log(data)
+        res.send(data)
+        if (err) throw err
+
+    })
+})
+
+
+
+module.exports = app
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,17 @@
 
-router.get("/exercises", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/exercises.html"));
-  });
 
-  router.get("/stats", (req, res) => {
+// Requires the path file for my HTML files
+
+var path = require("path");
+
+module.exports = function (app) {
+  // continue workout / new workout
+  app.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+ 
+  // get stats for our workouts
+  app.get("/stats", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
+};
